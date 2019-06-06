7b51864155ce37e576bc162defa7abd1ad170ae0
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-06 23:18:20 +0300

    completed defaultprops and state section in coincontainer

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class CoinContainer extends Component {
     static defaultProps = {
         coins: [
-            {side: 'heads', url: "https://tinyurl.com/react-coin-heads-jpg" },
-            {side: 'tails', url: "https://tinyurl.com/react-coin-tails-jpg"}
+            { side: 'heads', url: "https://tinyurl.com/react-coin-heads-jpg" },
+            { side: 'tails', url: "https://tinyurl.com/react-coin-tails-jpg" }
         ]
     }
 
@@ -16,7 +16,26 @@ class CoinContainer extends Component {
             nHeads: 0,
             nTails: 0
         }
-        
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+
+    flipCoin(){
+
+    }
+
+    handleClick(e){
+        this.flipCoin()
+    }
+    render(){
+        return(
+        <div className="CoinContainer">
+            <h2>Let's Flip A Coin!</h2>
+            <button onClick={this.handleClick}>Flip Me!</button>
+            <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
+
+        </div>
+        )
     }
 }
 
